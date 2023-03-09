ae860bc8a24c068dfa5e8b45f7ca6cc28c7f70a0
mouadse <mouadse@gmail.com>
2023-03-09 01:49:51 +0100

    I did rewrite the code to use asyncio and aiohttp library as a mental exercise to practice using both of them.

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
+import asyncio
 import os
 
-import requests
+import aiohttp
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Access the secrets using os.getenv()
 
-yandex_api_key = os.getenv("YANDEX_API_KEY")
+async def fetch_weather():
+    yandex_api_key = os.getenv("YANDEX_API_KEY")
+    access_key = yandex_api_key
+    headers = {"X-Meteum-API-Key": access_key}
+    query = """{
+        weatherByPoint(request: { lat: 28.49462128, lon: -11.32948112 }) {
+            now {
+                temperature
+            }
+        }
+    }"""
+    async with aiohttp.ClientSession() as session:
+        async with session.post('https://api.meteum.ai/graphql/query', headers=headers,
+                                json={'query': query}) as response:
+            current_weather = await response.json()
+            weather_now = current_weather['data']['weatherByPoint']['now']['temperature']
+            print(f"The weather now is {weather_now}°C")
 
-access_key = yandex_api_key
 
-headers = {"X-Meteum-API-Key": access_key}
+async def main():
+    await fetch_weather()
 
-query = """{
-  weatherByPoint(request: { lat: 28.49462128, lon: -11.32948112 }) {
-    now {
-      temperature
-    }
-  }
-}"""
 
-response = requests.post('https://api.meteum.ai/graphql/query', headers=headers, json={'query': query})
-
-current_weather = response.json()
-
-weather_now = current_weather['data']['weatherByPoint']['now']['temperature']
-
-print("The weather now is " + str(weather_now) + "°C")
+if __name__ == '__main__':
+    asyncio.run(main())
